6d004aeb08b1df628f307237841354f8d9d66ff2
Monika Klímová <monika.klimova@o2.cz>
2025-05-18 08:42:32 +0200

    Upload Event Planner project files

--- obj/Debug/net8.0/C#2planovac_udalosti.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#2planovac_udalosti.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#2planovac_udalosti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d706fad5a41e2fd1cadf0db645111de272fb8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b864f8acc362046cde69cbf4d7df433e378fa49b")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#2planovac_udalosti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#2planovac_udalosti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
